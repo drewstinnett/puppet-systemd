2f42e41cb5d592f381b5e0d13cafa0bc64040f1e
Drew Stinnett <drew.stinnett@duke.edu>
2014-02-09 16:49:27 -0500

    Prepping manifest for compatability

--- manifests/set_unit.pp
+++ manifests/set_unit.pp
@@ -3,33 +3,34 @@
 # =====================
 # Manage a systemd service file
 define systemd::set_unit(
-  $description  = absent,
-  $path         = absent,
-  $wants        = 'basic.target',
-  $after        = 'basic.target network.target',
+  $description   = absent,
+  $path          = absent,
+  $section_label = 'Unit',
+  $wants         = 'basic.target',
+  $after         = 'basic.target network.target',
 ){
 
   if $path == absent {
     fail('path required for systemd::set_unit')
   }
 
-  systemd::set_service_value{"unit-description":
+  systemd::set_service_value{'unit-description':
     path    => $path,
-    section => 'Unit',
+    section => $section_label,
     setting => 'Description',
     value   => $description
   }
 
-  systemd::set_service_value{"unit-wants":
+  systemd::set_service_value{'unit-wants':
     path    => $path,
-    section => 'Unit',
+    section => $section_label,
     setting => 'Wants',
     value   => $wants
   }
 
-  systemd::set_service_value{"unit-after":
+  systemd::set_service_value{'unit-after':
     path    => $path,
-    section => 'Unit',
+    section => $section_label,
     setting => 'After',
     value   => $after
   }
